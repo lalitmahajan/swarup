7c5e6162e5be132ada72ba27c3db58f9d64e0da1
WINDOWS-7 <WINDOWS-7@WINDOWS-7-PC>
2017-05-11 12:14:58 +0530

    first commit

---
+++ SpringRestClient/src/com/app/test/Test.java
@@ -0,0 +1,11 @@
+package com.app.test;
+
+public class Test {
+	static String uri="http://localhost:8779/RestEasyExample1/abc/hello/sayHello/lalit";
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
